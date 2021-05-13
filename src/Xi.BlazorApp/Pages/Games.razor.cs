a298c220d59f9eb3f0b0419014260d93da187031
Bart Kiers <bkiers@gmail.com>
2021-05-13 22:50:47 +0200

    Moved effect method

--- src/Xi.BlazorApp/Pages/Games.razor.cs
+++ src/Xi.BlazorApp/Pages/Games.razor.cs
@@ -1,10 +1,13 @@
 namespace Xi.BlazorApp.Pages
 {
-  using System.Linq;
+  using System;
+  using System.Collections.Generic;
+  using System.Threading.Tasks;
   using Fluxor;
   using Microsoft.AspNetCore.Components;
   using Xi.BlazorApp.Stores.Features.Games.Actions.LoadGames;
   using Xi.BlazorApp.Stores.States;
+  using Xi.Models.Game;
 
   public partial class Games
   {
@@ -16,19 +19,36 @@ namespace Xi.BlazorApp.Pages
 
     [ReducerMethod]
     public static GamesState ReduceLoadGamesAction(GamesState state, LoadGamesAction action) =>
-      new GamesState(true, null, null);
+      new(true, null, null);
 
     [ReducerMethod]
     public static GamesState ReduceLoadGamesSuccessAction(GamesState state, LoadGamesSuccessAction action) =>
-      new GamesState(false, null, action.Games);
+      new(false, null, action.Games);
 
     [ReducerMethod]
     public static GamesState ReduceLoadGamesFailureAction(GamesState state, LoadGamesFailureAction action) =>
-      new GamesState(false, action.ErrorMessage, null);
+      new(false, action.ErrorMessage, null);
+
+    [EffectMethod]
+    public static async Task HandleAsync(LoadGamesAction action, IDispatcher dispatcher)
+    {
+      await Task.Delay(TimeSpan.FromMilliseconds(2000));
+
+      var games = new List<Game>
+      {
+        new(),
+        new(),
+        new(),
+        new(),
+        new(),
+      };
+
+      dispatcher.Dispatch(new LoadGamesSuccessAction(games));
+    }
 
     protected override void OnInitialized()
     {
-      if (this.GamesState.Value.Games?.Any() != true)
+      if (this.GamesState.Value.Games == null)
       {
         this.Dispatcher.Dispatch(new LoadGamesAction());
       }
