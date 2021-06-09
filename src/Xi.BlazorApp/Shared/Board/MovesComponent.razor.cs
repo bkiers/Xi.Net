520d76a7bb498f49fdd0e4cb3e190ed17dcee401
Bart Kiers <bkiers@gmail.com>
2021-06-09 21:35:09 +0200

    More MudBlazor

--- src/Xi.BlazorApp/Shared/Board/MovesComponent.razor.cs
+++ src/Xi.BlazorApp/Shared/Board/MovesComponent.razor.cs
@@ -1,11 +1,14 @@
 namespace Xi.BlazorApp.Shared.Board
 {
+  using System;
+  using System.Collections.Generic;
   using System.Threading.Tasks;
   using Fluxor;
   using Microsoft.AspNetCore.Components;
   using Toolbelt.Blazor.HotKeys;
   using Xi.BlazorApp.Models;
   using Xi.BlazorApp.Stores.Features.Game.Actions.Moves;
+  using Xi.Models.Game;
 
   public partial class MovesComponent
   {
@@ -28,6 +31,22 @@ namespace Xi.BlazorApp.Shared.Board
 
       await base.OnInitializedAsync();
     }
+
+
+    private List<Tuple<int, int, int>> MoveNumbers()
+    {
+      var moves = new List<Tuple<int, int, int>>();
+
+      for (var i = 0; i < this.GameModel.Game.Moves.Count; i += 2)
+      {
+        var moveNumber = ((i + 1) / 2) + 1;
+
+        moves.Add(new Tuple<int, int, int>(moveNumber, i, i + 1));
+      }
+
+      return moves;
+    }
+
 
     private void First()
     {
