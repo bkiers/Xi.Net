bdedd321462ad6689d0e6d514f7839623f645f59
Bart Kiers <bkiers@gmail.com>
2021-10-09 20:42:42 +0200

    Loading env config

--- src/Xi.BlazorApp/Program.cs
+++ src/Xi.BlazorApp/Program.cs
@@ -1,6 +1,8 @@
 namespace Xi.BlazorApp
 {
+  using System;
   using Microsoft.AspNetCore.Hosting;
+  using Microsoft.Extensions.Configuration;
   using Microsoft.Extensions.Hosting;
 
   public class Program
@@ -12,10 +14,18 @@ namespace Xi.BlazorApp
 
     public static IHostBuilder CreateHostBuilder(string[] args)
     {
-      return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
-      {
-        webBuilder.UseStartup<Startup>();
-      });
+      return Host.CreateDefaultBuilder(args)
+        .ConfigureAppConfiguration((hostingContext, config) =>
+        {
+          var settingsFile = $"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json";
+
+          Console.WriteLine($">>> Loading: {settingsFile}");
+          config.AddJsonFile(settingsFile);
+        })
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+          webBuilder.UseStartup<Startup>();
+        });
     }
   }
 }
