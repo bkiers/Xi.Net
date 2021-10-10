2813cf4532501603c471f174def94a05fd67386a
Bart Kiers <bkiers@gmail.com>
2021-10-10 11:42:24 +0200

    Create and run production build

--- run-prod.sh
+++ run-prod.sh
@@ -6,6 +6,14 @@
 #
 # In `/etc/rc.local`, for example:
 #   su - SOME_USER -c "screen -dmS xi_net"
-#   su - SOME_USER -c "screen -S xi_net -X stuff 'cd /path/to/xi && ./run-prod.sh\n'"
+#   su - SOME_USER -c "screen -S xi_net -X stuff 'cd /path/to/Xi.Net && ./run-prod.sh\n'"
 
-dotnet run --project ./src/Xi.BlazorApp -- Production
+# Clear any previous binaries
+rm -rf ./src/Xi.BlazorApp/bin/Release
+
+# Create a production release
+dotnet publish -c Release -p:UseAppHost=false
+
+# Run the production build
+cd src/Xi.BlazorApp/bin/Release/net5.0/publish
+dotnet Xi.BlazorApp.dll --urls "https://localhost:9900" -- Production
