9e89a29cd51b0c4e6ab80778f97834f32b9e1ee3
Bart Kiers <bkiers@gmail.com>
2021-10-10 13:17:35 +0200

    Graceful shutdown

--- src/Xi.BlazorApp/Program.cs
+++ src/Xi.BlazorApp/Program.cs
@@ -3,28 +3,27 @@ namespace Xi.BlazorApp
   using System;
   using Microsoft.AspNetCore.Hosting;
   using Microsoft.Extensions.Configuration;
+  using Microsoft.Extensions.DependencyInjection;
   using Microsoft.Extensions.Hosting;
 
   public class Program
   {
     public static void Main(string[] args)
     {
-      CreateHostBuilder(args).Build().Run();
+      var host = CreateHostBuilder(args).Build();
+
+      host.Services.GetRequiredService<IHostApplicationLifetime>()
+        .ApplicationStopped.Register(() =>
+        {
+          Console.WriteLine("Xi.Net is shut down");
+        });
+
+      host.Run();
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args)
     {
       return Host.CreateDefaultBuilder(args)
-        .ConfigureAppConfiguration((_, config) =>
-        {
-          if (args.Length == 1)
-          {
-            var settingsFile = $"appsettings.{args[0]}.json";
-
-            Console.WriteLine($">>> Loading: {settingsFile}");
-            config.AddJsonFile(settingsFile);
-          }
-        })
         .ConfigureWebHostDefaults(webBuilder =>
         {
           webBuilder.UseStartup<Startup>();
