cb0e011175ad1b5dcf962cfca9c9b667bbfd674b
Bart Kiers <bkiers@gmail.com>
2021-10-13 19:47:26 +0200

    Remove bash function

--- check-changes.sh
+++ check-changes.sh
@@ -9,17 +9,6 @@
 #   su - SOME_USER -c "screen -dmS xi_net_chk"
 #   su - SOME_USER -c "screen -S xi_net_chk -X stuff 'cd /path/to/Xi.Net && ./check-changes.sh\n'"
 
-function restart_xi {
-  # Send a CTRL+C to the `xi_net` screen
-  screen -S xi_net -X stuff "^C"
-  
-  # Wait a bit so that the process can stop
-  sleep 5
-
-  # Restart the Xi app again
-  screen -S xi_net -X stuff './run-prod.sh\n'
-}
-
 while [ true ]
 do
   RUNNING=$(ps aux | grep Xi.BlazorApp | grep :9900)
@@ -28,7 +17,8 @@ do
   then
     echo "Xi has stopped, restarting!"
     
-    restart_xi
+    # Restart the Xi app again
+    screen -S xi_net -X stuff './run-prod.sh\n'
 
     # Wait 60 seconds
     sleep 60
@@ -46,7 +36,14 @@ do
     # Pull the changes
     git pull --no-edit origin master
 
-    restart_xi
+    # Send a CTRL+C to the `xi_net` screen
+    screen -S xi_net -X stuff "^C"
+    
+    # Wait a bit so that the process can stop
+    sleep 5
+
+    # Restart the Xi app again
+    screen -S xi_net -X stuff './run-prod.sh\n'
   fi
 
   # Wait 60 seconds
